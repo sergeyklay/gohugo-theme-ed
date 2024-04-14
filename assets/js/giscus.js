43c12205069d0c0c4a3b3c2a6bcf30a991655d84
Serghei Iakovlev <egrep@protonmail.ch>
2024-04-14 17:57:42 +0200

    Provide an ability to use comments form

---
+++ assets/js/giscus.js
@@ -0,0 +1,37 @@
+import {
+  category,
+  categoryId,
+  emitMetadata,
+  inputPosition,
+  lang,
+  mapping,
+  reactionsEnabled,
+  repo,
+  repoId,
+  strict,
+  theme
+} from '@params';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const giscusAttributes = {
+    'src': 'https://giscus.app/client.js',
+    'data-repo': repo,
+    'data-repo-id': repoId,
+    'data-category': category,
+    'data-category-id': categoryId,
+    'data-mapping': mapping,
+    'data-strict': strict,
+    'data-reactions-enabled': reactionsEnabled,
+    'data-emit-metadata': emitMetadata,
+    'data-input-position': inputPosition,
+    'data-theme': theme,
+    'data-lang': lang,
+    'crossorigin': 'anonymous',
+    'async': ''
+  };
+
+  // Dynamically create script tag
+  const giscusScript = document.createElement('script');
+  Object.entries(giscusAttributes).forEach(([key, value]) => giscusScript.setAttribute(key, value));
+  document.getElementById('giscus').appendChild(giscusScript);
+});
