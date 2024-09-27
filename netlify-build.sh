70574ea3b68be6a6d4996867ae9c07dfbadcafc4
Serghei Iakovlev <egrep@protonmail.ch>
2024-09-27 20:13:33 +0200

    Try another way, using js

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-echo "Installing yq"
-
-VERSION=v4.44.3
-BINARY=yq_linux_amd64
-
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |\
-  tar xz && mv ${BINARY} ./yq
-
-chmod +x ./yq
-./yq --version
-
-echo "Patch config file for demo site"
-
-./yq -i '.privacy.googleTagManager.disable = false' exampleSite/config/_default/params.yaml
-./yq -i '.services.googleTagManager.id = "GTM-W8D5W642"' exampleSite/config/_default/params.yaml
-
+npm ./netlify-build.js
 cat exampleSite/config/_default/params.yaml
+
+#echo "Installing yq"
+#
+#VERSION=v4.44.3
+#BINARY=yq_linux_amd64
+#
+#wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |\
+#  tar xz && mv ${BINARY} ./yq
+#
+#chmod +x ./yq
+#./yq --version
+#
+#echo "Patch config file for demo site"
+#
+#./yq -i '.privacy.googleTagManager.disable = false' exampleSite/config/_default/params.yaml
+#./yq -i '.services.googleTagManager.id = "GTM-W8D5W642"' exampleSite/config/_default/params.yaml
+#
+#cat exampleSite/config/_default/params.yaml
